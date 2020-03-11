ab366f0bb4fa6ba24e7d0e182df4ce19fe6e8b9e
Oscar Mier <voscarmv@gmail.com>
2020-03-11 10:47:07 -0600

    Added UNIX fortune functionality.

--- slack-stackbot/commands/fortune.rb
+++ slack-stackbot/commands/fortune.rb
@@ -1,8 +1,10 @@
+require_relative '../../lib/Fortune.rb'
+
 module SlackStackbot
   module Commands
     class Search < SlackRubyBot::Commands::Base
       command 'fortune' do |client, data, _match|
-        client.say(channel: data.channel, text: "Here I tell a fortune!")
+        client.say(channel: data.channel, text: FortuneGem.give_fortune)
       end
     end
   end
