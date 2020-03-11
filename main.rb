7a2ad5d2480db63ccb3e7b5aa0ca5adc8391b9cb
Oscar Mier <voscarmv@gmail.com>
2020-03-11 13:28:21 -0600

    Developed basics for testing fortune with rspec

--- main.rb
+++ main.rb
@@ -1,6 +1,46 @@
 #!/usr/bin/env ruby
 require_relative 'lib/StackOverflow.rb'
 require_relative 'lib/Fortune.rb'
-puts 'Hello, World!'
-puts StackOverflow::Search.new.questions("html api")
-UnixFortunes::Fortune.new.give_fortune
+#puts StackOverflow::Search.new.questions("html api")
+fortune = UnixFortunes::Fortune.new.give_fortune
+
+fortune = 'For there are moments when one can neither think nor feel.  And if one can
+neither think nor feel, she thought, where is one?
+		-- Virginia Woolf, "To the Lighthouse"
+
+	[Quoted in "VMS Internals and Data Structures", V4.4, when
+   referring to powerfail recovery.]'
+   
+  #  puts fortune
+  #  puts Regexp.quote(fortune)
+  #  puts fortune =~ /Regexp.quote(fortune)/m
+  if fortune =~ /Regexp.quote(fortune)/m
+      puts "FOUND!!!"
+   end
+
+myfile = File.open('lib/fortunes')
+# contents = Regexp.quote(myfile.read)
+
+# # puts contents
+
+# if contents.include?(fortune)
+#   puts "FOUNDIT!"
+# end
+
+file = 'hello
+world
+how
+are
+you?'
+
+file = myfile.read
+findthis = UnixFortunes::Fortune.new.give_fortune
+
+# findthis = 'Nas'
+puts findthis
+
+if file =~ /#{Regexp.quote(findthis)}/m then
+  puts "Found it!"
+end
+
+# puts File.open('lib/fortunes').grep(Regexp.new(Regexp.quote(fortune), Regexp::MULTILINE | Regexp::EXTENDED))
